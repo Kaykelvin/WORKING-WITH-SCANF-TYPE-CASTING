f1b1c4bd9041639d8a04862922b69f34c93b8809
kabalikelvin12345@gmail.com <kabalikelvin12345@gmail.com>
2025-05-21 23:39:38 +0300

    branch1: second commit

--- 1.Average_Sales_Calculation.c
+++ 1.Average_Sales_Calculation.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main() {
+    int total_sales;
+    int days;
+    double average_sales;
+
+    printf("Enter total sales (UGX): ");
+    scanf("%d", &total_sales);
+
+    printf("Enter number of days: ");
+    scanf("%d", &days);
+
+    average_sales = (double)total_sales / days;
+
+    printf("Average Sales per Day: %.2lf UGX\n", average_sales);
+
+    return 0;
+}
